5c17b5cb251a029f71e9649d75e8b8f4ac39edad
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-19 17:52:14 +0000

    styling vending machine component

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './VendingMachine.css'
 
 export default class VendingMachine extends Component {
     render() {
-        return (<div>
-            <h1>Vending Machine Component</h1>
-        </div>)
+        return (
+            <div className='VendingMachine'>
+                <img src='https://i.dlpng.com/static/png/5096647-cartoon-red-vending-machine-cartoon-vector-cartoon-red-red-png-vending-machine-png-650_651_preview.png' />
+                <ul>
+                    <li>
+                        <NavLink className='VendingMachine-NavLink' to='/doritos'>Doritos</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className='VendingMachine-NavLink' to='/oreo'>Oreo</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className='VendingMachine-NavLink' to='/coca-cola'>Coca-Cola</NavLink>
+                    </li>
+                </ul>
+            </div>
+        )
     }
 }
 

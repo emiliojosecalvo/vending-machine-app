17da50c6266de95dcc32e00a28cf1c647acb092c
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-19 16:45:16 +0000

    adding routes and components

---
+++ src/VendingMachine.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class VendingMachine extends Component {
+    render() {
+        return (<div>
+            <h1>Vending Machine Component</h1>
+        </div>)
+    }
+}
+
